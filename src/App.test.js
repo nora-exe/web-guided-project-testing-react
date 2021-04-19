030976260ffa6c8a87d6f216b22be7a4dcb057d7
Dustin Myers <dustint314@gmail.com>
2021-04-19 11:59:34 -0600

    wip

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { fetchMissions as mockFetchMissions } from "./api/fetchMissions";
+
 import App from "./App";
+
+// mock the function that makes the API request
+// 1 - build the mock function
+jest.mock("./api/fetchMissions");
+console.log(mockFetchMissions);
 
 // When the app comp renders and the "get data" button is clicked
 // the api call is made and the missions list is rendered when the api
 // promise is resolved
 test("missions data is rendered after the API call is resolved", async () => {
+  // 2. make the mock function return a promise, then return data
   render(<App />);
 
   // click on the button
