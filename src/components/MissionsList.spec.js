0bc4f2449eef91e0d0253146bf1df9159ab22c8b
Dustin Myers <dustint314@gmail.com>
2021-04-19 11:31:54 -0600

    wip

---
+++ src/components/MissionsList.spec.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import MissionsList from "./MissionsList";
+
+// When the component receives missions data
+// then it displays the data in the DOM
+test("displays missions data when passed in as a prop", () => {
+  // render(<MissionsList />)
+});
