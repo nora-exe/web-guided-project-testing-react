e8b5028467070394844eaded020c6641488994b2
Dustin Myers <dustint314@gmail.com>
2021-04-19 11:12:55 -0600

    wip

--- src/components/MissionForm.test.js
+++ src/components/MissionForm.test.js
@@ -46,5 +46,5 @@ test("the function prop is called when Get Data is clicked", () => {
   userEvent.click(button);
 
   // assert
-  expect(mockGetData).toHaveBeenCalled();
+  expect(mockGetData).not.toHaveBeenCalled();
 });
