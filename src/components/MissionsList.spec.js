aa502c1a22d96f51a35151628c50912295e1a6c5
Dustin Myers <dustint314@gmail.com>
2021-04-19 11:38:02 -0600

    wip

--- src/components/MissionsList.spec.js
+++ src/components/MissionsList.spec.js
@@ -3,7 +3,11 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import MissionsList from "./MissionsList";
 
-const testData = [{ mission_id: "123", mission_name: "Thaicom" }];
+const testData = [
+  { mission_id: "123", mission_name: "Thaicom" },
+  { mission_id: "1234", mission_name: "Telstar" },
+  { mission_id: "1235", mission_name: "Iridium NEXT" },
+];
 
 // When the component receives missions data
 // then it displays the data in the DOM
