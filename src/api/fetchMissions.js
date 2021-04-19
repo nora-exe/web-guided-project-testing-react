af78ab9e0758ad48a519a16a0d666fa6ff750d3e
Dustin Myers <dustint314@gmail.com>
2021-04-19 11:57:17 -0600

    wip

--- src/api/fetchMissions.js
+++ src/api/fetchMissions.js
@@ -3,11 +3,11 @@ import axios from "axios";
 export const fetchMissions = () => {
   return axios
     .get("https://api.spacexdata.com/v3/missions")
-    .then(res => {
+    .then((res) => {
       console.log(res);
       return res;
     })
-    .catch(err => {
+    .catch((err) => {
       console.error("error fetching data from api, err: ", err.message);
       return err;
     });
