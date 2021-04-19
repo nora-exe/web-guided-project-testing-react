7ce682d1019bd2e284916f74e5c809dc6823c77a
Dustin Myers <dustint314@gmail.com>
2021-04-19 11:53:24 -0600

    wip

--- src/App.test.js
+++ src/App.test.js
@@ -4,4 +4,5 @@ import userEvent from "@testing-library/user-event";
 import App from "./App";
 
 // When the app comp renders and the "get data" button is clicked
-// the api call is made and the missions list is rendered when the api promise is resolved
+// the api call is made and the missions list is rendered when the api
+// promise is resolved
