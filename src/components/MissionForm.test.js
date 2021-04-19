742b1e1d93fa6b4073288a9a26977090122a812e
Dustin Myers <dustint314@gmail.com>
2021-04-19 10:23:10 -0600

    wip

--- src/components/MissionForm.test.js
+++ src/components/MissionForm.test.js
@@ -1,3 +1,9 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
 import MissionForm from "./MissionForm";
+
+// Mission form renders with no errors, when it first mounts
+test("renders without errors", () => {
+  render(<MissionForm />);
+});
