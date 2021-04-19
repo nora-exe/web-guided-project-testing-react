2f3d47f78f0b5a97212aa44f920203c355b82de4
Dustin Myers <dustint314@gmail.com>
2021-04-19 10:48:28 -0600

    wip

--- src/components/MissionForm.test.js
+++ src/components/MissionForm.test.js
@@ -3,25 +3,29 @@ import { render, screen } from "@testing-library/react";
 import { userEvent } from "@testing-library/user-event";
 import MissionForm from "./MissionForm";
 
-// Mission form renders with no errors, when it first mounts
-test("renders without errors", () => {
-  render(<MissionForm />);
-});
+describe("MissionForm", () => {
+  // Mission form renders with no errors, when it first mounts
+  test("renders without errors", () => {
+    render(<MissionForm />);
+  });
 
-// When the component receives true for "isFetchingData" prop,
-// then the loading message appears in place of the button.
-test("renders loading message when isFetchingData changes to true", () => {
-  // arrange - render the component with the starting props
-  const { rerender } = render(<MissionForm isFetchingData={false} />);
+  // When the component receives true for "isFetchingData" prop,
+  // then the loading message appears in place of the button.
+  test("renders loading message when isFetchingData changes to true", () => {
+    // arrange - render the component with the starting props
+    const { rerender } = render(<MissionForm isFetchingData={false} />);
 
-  // assert - the Get Data button is rendered before the props change
-  expect(screen.queryByText(/we are fetching data/i)).toBeNull();
-  expect(screen.getByRole("button", { name: /get data/i })).toBeInTheDocument();
+    // assert - the Get Data button is rendered before the props change
+    expect(screen.queryByText(/we are fetching data/i)).toBeNull();
+    expect(
+      screen.getByRole("button", { name: /get data/i })
+    ).toBeInTheDocument();
 
-  // act - rerender the component with new props
-  rerender(<MissionForm isFetchingData={true} />);
+    // act - rerender the component with new props
+    rerender(<MissionForm isFetchingData={true} />);
 
-  // assert - check that the loading message is rendered
-  expect(screen.getByText(/we are fetching data/i)).toBeInTheDocument();
-  expect(screen.queryByRole("button")).toBeNull();
+    // assert - check that the loading message is rendered
+    expect(screen.getByText(/we are fetching data/i)).toBeInTheDocument();
+    expect(screen.queryByRole("button")).toBeNull();
+  });
 });
