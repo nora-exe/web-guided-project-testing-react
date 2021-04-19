50653ff8dd3fd82e3d93f7de8d76a9ec2ca9e2fb
Dustin Myers <dustint314@gmail.com>
2021-04-19 11:37:14 -0600

    wip

--- src/components/MissionsList.spec.js
+++ src/components/MissionsList.spec.js
@@ -3,8 +3,12 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import MissionsList from "./MissionsList";
 
+const testData = [{ mission_id: "123", mission_name: "Thaicom" }];
+
 // When the component receives missions data
 // then it displays the data in the DOM
 test("displays missions data when passed in as a prop", () => {
-  render(<MissionsList />);
+  const { rerender } = render(<MissionsList missions={[]} />);
+
+  rerender(<MissionsList missions={testData} />);
 });
