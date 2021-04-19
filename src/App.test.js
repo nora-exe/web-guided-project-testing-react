4e8a6788b25cf85b9ac996a5a5d75e58ac89cd08
Dustin Myers <dustint314@gmail.com>
2021-04-19 11:52:36 -0600

    wip

---
+++ src/App.test.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
+
+// When the app comp renders and the "get data" button is clicked
+// the api call is made and the missions list is rendered when the api promise is resolved
